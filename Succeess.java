17c74a46ad5725f96cd4aac590ad4cd63257979b
nithu33 <42604896+nithu33@users.noreply.github.com>
2018-08-23 19:05:34 +0530

    Add files via upload

---
+++ Succeess.java
@@ -0,0 +1,19 @@
+import compo.*;
+public class Succeess {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Feature h=new Feature();           //creating object for 2 class
+h.setname("yamaha");
+h.setcolour("green");
+h.setwheel(2);
+h.setyear(2005);
+h.bikefeatures();
+h.setsecure();
+h.setquality();
+h.setstart();
+h.setstop();
+
+	}
+
+}
