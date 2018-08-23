1a4ae21bcd99f433f26e6abf11c0b5d44a771380
nithu33 <42604896+nithu33@users.noreply.github.com>
2018-08-23 19:03:59 +0530

    Add files via upload

---
+++ Types.java
@@ -0,0 +1,20 @@
+import compo.*;
+public class Types {
+	public void quality()       // member function 
+	{
+		System.out.println("it has automatic break \t ");
+	}
+	public void secure()
+	{
+		System.out.println("the bike has automatic sound to secure the things in the bike\t ");
+	}
+	public void start()
+	{
+		System.out.println("the engine has been started:\t");
+	}
+	public void stop()
+	{
+		System.out.println("the enginee has been stopped :\t");
+	}
+
+}
